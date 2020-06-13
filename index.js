402af19afb214dfc501faf748a1201121a871e12
Shi <g.vandenbiggelaar@gmail.com>
2020-06-13 12:38:00 +0200

    initiative and procfile

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const prefix = "~";
 const app = express();
 
 totalRolled = 0;
+players = new Array();
+enemyTotal = 0;
+stack = new Array();
+currentTurn = "";
 
 bot.login(TOKEN);
 
@@ -23,7 +27,87 @@ bot.on('message', (message) => {
         message.channel.send(`${message.author.username}, you rolled: ${totalRolled}`);
         message.channel.send(`${result}`);
     }
+
+    else if(message.content.includes(`${prefix}addplayer`) && isRole(message, "Maid")){
+        let arg = isolateArgument(message.content);
+        addPlayer(arg);
+        message.channel.send(`Added Player: ${arg}`);
+    }
+
+    else if(message.content.includes(`${prefix}playerlist`) && isRole(message, "Maid")){
+        message.channel.send(`The current players are: \n${playerList()}`);
+    }
+
+    else if(message.content.includes(`${prefix}resetplayers`) && isRole(message, "Maid")){
+        players = new Array();
+        message.channel.send('Reset the Player List');
+    }
+
+    else if(message.content.includes(`${prefix}setenemies`) && isRole(message, "Maid")){
+        let arg = isolateArgument(message.content);
+        enemyTotal = arg;
+        message.channel.send(`Enemy tokens set to: ${arg}`);
+    }
+
+    else if(message.content.includes(`${prefix}startround`) && isRole(message, "Maid")){
+        newRound();
+        message.channel.send(`Created new Initiative Stack with:\n${playerList()} ${enemyTotal} Enemy Token(s)`);
+    }
+
+    else if(message.content.includes(`${prefix}nextturn`) && isRole(message, "Maid")){
+        nextTurn();
+        if (currentTurn === 'End of Round') message.channel.send(`**End of Round!**`);
+        else message.channel.send(`The current initiative goes to: \n${currentTurn}`);
+    }
+
+    else if(message.content.includes(`${prefix}delay`) && isRole(message, "Maid")){
+        delayTurn();
+        message.channel.send(`Okay! ${currentTurn}'s turn has been delayed`);
+    }  
 });
+
+function delayTurn(){
+    stack.push(currentTurn);
+}
+
+function nextTurn(){
+    let index = Math.floor(Math.random() * stack.length);
+
+    currentTurn = stack[index];
+
+    stack.splice(index, 1);
+}
+
+function newRound(){
+    stack = new Array();
+
+    players.forEach(player => {
+        stack.push(player);
+        stack.push(player);
+    });
+
+    for (let index = 0; index < enemyTotal; index++) {
+        stack.push('Enemy');
+    }
+
+    stack.push('End of Round');
+}
+
+function playerList(){
+    let output = "";
+    players.forEach(player => {
+        output += `${player}\n`;
+    });
+    return output;
+}
+
+function addPlayer(arg){
+    players.push(arg);
+}
+
+function isRole(msg, role){
+    return msg.member.roles.cache.find(r => r.name === role);
+}
 
 function isolateArgument(input) {
     let command = input.substring(0, input.indexOf(' '));
